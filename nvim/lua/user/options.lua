a7b1b235db01d9d68d285255fc3116064425415f
Paweł Bielecki <bieleckipawel10@gmail.com>
2023-02-23 17:28:16 +0100

    Change nvim config

---
+++ nvim/lua/user/options.lua
@@ -0,0 +1,29 @@
+local options = {
+	backup = false,
+	clipboard = "unnamedplus",
+	hlsearch = false,
+	ignorecase = true,
+	mouse = "a",
+	showmode = false,
+	smartindent = true,
+	splitbelow = true,
+	splitright = true,
+	swapfile = false,
+	termguicolors = true,
+	undofile = true,
+	expandtab = true,
+	shiftwidth = 4,
+	tabstop = 4,
+	number = true,
+	relativenumber = true,
+    numberwidth = 4,
+    signcolumn = "number",
+    wrap = false,
+    scrolloff = 8,
+}
+
+for k, v in pairs(options) do
+	vim.opt[k] = v
+end
+
+
