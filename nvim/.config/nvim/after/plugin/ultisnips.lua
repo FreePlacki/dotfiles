525cd7756bbf5ff26f28f3eea76ffd411a053ba4
FreePlacki <bieleckipawel10@gmail.com>
2024-10-26 22:26:41 +0200

    Fix diagnostic jumping

--- nvim/.config/nvim/after/plugin/ultisnips.lua
+++
@@ -1,6 +0,0 @@
-vim.api.nvim_set_keymap('i', '<c-j>', '<C-R>=UltiSnips#ExpandSnippetOrJump()<CR>', { noremap = true, silent = true })
--- vim.g.UltiSnipsExpandTrigger = '<c-j>' -- doesn;'t work!
--- vim.g.UltiSnipsJumpForwardTrigger = '<Tab>'
--- vim.g.UltiSnipsJumpBackwardTrigger = '<S-Tab>'
--- vim.g.UltiSnipsSnippetsDir="~/.config/nvim/my_snippets/"
--- vim.g.UltiSnipsSnippetDirectories={"UltiSnips", "my_snippets"}
